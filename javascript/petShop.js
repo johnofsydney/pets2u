5228a9130d562c896a2e6adf3b55c72dadf9cb33
John Coote <john_coote@rocketmail.com>
2021-06-13 13:23:27 +1000

    refactor JS, add comments etc

--- javascript/petShop.js
+++ javascript/petShop.js
@@ -5,50 +5,83 @@ export const compute = (animals) => {
 
   const requiredBoxes = [];
 
-  // how many snake boxes
-  const snakeBoxes = animals.reduce( (count, animal) => {
-    let a = (animal == 'S') ? 1 : 0;
-    return (count + a)
-  }, 0)
+  // transform array to object with quantities
+  animals = transform(animals)
 
-  // how many small animals
-  let smallAnimals = animals.reduce( (count, animal) => {
-    let a = (animal == 'R' || animal == 'H') ? 1 : 0;
-    return (count + a)
-  }, 0)
-
-  // if there are small animals, squeeze them into the snake boxes
-  // store the number of remaining small animals
-  smallAnimals = smallAnimals > snakeBoxes ? smallAnimals - snakeBoxes : 0;
+  // consolidate animals
+  animals = consolidate(animals)
 
   // calculates Boxes for snakes and companions
-  for (let index = 0; index < snakeBoxes; index++) {
+  for (let index = 0; index < animals.snakes.quantity; index++) {
     requiredBoxes.push('B3')
   }
 
-  // how many medium animals
-  let mediumAnimals = animals.reduce( (count, animal) => {
-    let a = (animal == 'M') ? 1 : 0;
-    return (count + a)
-  }, 0)
-
-
-  // 3 - depends on how many animals we have
-  let unallocatedArea = (smallAnimals * 400) + (mediumAnimals * 800)
+  // remaining boxes depends on how much area we have to fill
+  let unallocatedArea = (
+    (animals.smallAnimals.area) + (animals.mongeese.area)
+    )
     while (unallocatedArea > 0) {
-    if (unallocatedArea > 800) {
+    if (unallocatedArea > BOX_SIZES.B2) {
       requiredBoxes.push('B3')
-      unallocatedArea = unallocatedArea - 1600
+      unallocatedArea = unallocatedArea - BOX_SIZES.B3
     }
-    else if (unallocatedArea >= 800) {
+    else if (unallocatedArea >= BOX_SIZES.B2) {
       requiredBoxes.push('B2')
-      unallocatedArea = unallocatedArea - 800
+      unallocatedArea = unallocatedArea - BOX_SIZES.B2
     }
-    else if (unallocatedArea = 400) {
+    else if (unallocatedArea = BOX_SIZES.B1) {
       requiredBoxes.push('B1')
-      unallocatedArea = unallocatedArea - 400
+      unallocatedArea = unallocatedArea - BOX_SIZES.B1
     }
   }
 
-  return(requiredBoxes.sort());
+  return(requiredBoxes.sort())
 };
+
+// input an array of leters representing animals
+// return: an object with
+//    keys: all possible animal letters,
+//    values: the quantities of each animal in the input
+const transform = (sentence) => {
+  // adapted from my solution to word-count exercise:
+  // https://github.com/johnofsydney/exercisms/tree/master/javascript/word-count
+  return (
+    sentence.reduce( (result, word) => {
+        result[word] ? result[word]++ : result[word] = 1
+        return(result)
+      }, {S: 0, M: 0, R: 0, H: 0})
+  );
+};
+
+// input: object representing quantities of each animal
+// output: object with actual quantites of snakes and mongeese
+// and the combined total of small animals that remain after allocating one
+// small animal per snake
+const consolidate = (animals) => {
+  let snakes = animals['S'];
+  let mongeese = animals['M'];
+  let smallAnimals = animals['H']+ animals['R'];
+
+  // we can squeeze a small animal in with each snake
+  let remainingSmallAnimals = smallAnimals > snakes ? smallAnimals - snakes : 0;
+
+  return(
+    {
+      snakes: {
+        quantity: snakes, area: snakes * 1200
+      },
+      mongeese: {
+        quantity: mongeese, area: mongeese * 800
+      },
+      smallAnimals: {
+        quantity: remainingSmallAnimals, area: remainingSmallAnimals * 400
+      }
+    }
+  )
+}
+
+const BOX_SIZES = {
+  B1: 400,
+  B2: 800,
+  B3: 1600
+}
--- javascript/readme.md
+++ javascript/readme.md
@@ -1,11 +1,15 @@
 NOTES:
 
 First commit, JS
-OK for a start. Did not pass [Snake, Mongoose] test. wants to break the mongoose into two boxes.
+- OK for a start. Did not pass [Snake, Mongoose] test. wants to break the mongoose into two boxes.
 Time estimate:
-30 mins to set up tests / code file and fgit repository
-90 mins to code solution
+- 30 mins to set up tests / code file and fgit repository
+- 90 mins to code solution
 
 All Tests Pass
-Tests Pass, needs a refactor
+- Tests Pass, needs a refactor
 Time Estimate: + 60 minutes
+
+Refactor complete
+Time Estimate: + 45 minutes
+
